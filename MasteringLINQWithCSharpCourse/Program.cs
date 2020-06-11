fd7949580f0f67ebf0641290ec3b5b619820fdab
awais <asaeed897@outlook.com>
2020-06-11 22:42:11 +0500

    Completed Lesson 5 Implementing IEnumerable<T>

--- MasteringLINQWithCSharpCourse/Program.cs
+++ MasteringLINQWithCSharpCourse/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,71 @@ using System.Threading.Tasks;
 
 namespace MasteringLINQWithCSharpCourse
 {
+    public class Params : IEnumerable<int>
+    {
+        private int a, b, c;
+
+        public Params(int a, int b, int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            yield return a;
+            yield return b;
+            yield return c;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+    }
+
+    public class Person
+    {
+        private string firstName, middleName, lastName;
+
+        public Person(string firstName, string middleName, string lastName)
+        {
+            this.firstName = firstName;
+            this.middleName = middleName;
+            this.lastName = lastName;
+        }
+
+        public IEnumerable<string> Names
+        {
+            get
+            {
+                yield return firstName;
+                yield return middleName;
+                yield return lastName;
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            // 4. IEnumerable<T>
+            var p = new Params(1, 2, 3);
+
+            foreach (var x in p)
+            {
+                Console.WriteLine(x);
+            }
+
+            var person = new Person("Vladimir", "Ilyitch", "Lenin");
+
+            foreach (var name in person.Names)
+            {
+                Console.WriteLine(name);
+            }
+            // 5. Implementing IEnumerable<T>
 
         }
     }
