554f2535ae86ef8fca13abe56975f3b0d2ec94b1
awais <asaeed897@outlook.com>
2020-06-12 23:16:16 +0500

    Completed Lesson 39 LINQ support in Resharper and Rider

--- MasteringLINQWithCSharpCourse/Program.cs
+++ MasteringLINQWithCSharpCourse/Program.cs
@@ -5,16 +5,46 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace MasteringLINQWithCSharpCourse
 {
 
     class Program
     {
+        void Process(IEnumerable<Human> people)
+        {
+            var ppl = people as IList<Human> ?? people.ToList();
+            var kids = ppl.Where(p => p.Age < 13);
+            var numberOfTeens = ppl.Count(p => p.Age >= 13 && p.Age <= 19);
+        }
+
+        IEnumerable<Human> GetTeenagersNamedAdam(IEnumerable<Human> people)
+        {
+            var result = new List<Human>();
+            foreach (var p in people)
+            {
+                if (p.Age >= 13 && p.Age <= 19)
+                    if (p.Name == "Adam")
+                        result.Add(p);
+            }
+            return result;
+        }
+
+        void XmlDemo()
+        {
+            var x = new XElement("foo", new XElement("bar")).ToString();
+        }
         static void Main(string[] args)
         {
 
-            // 38. LINQ to XML
+            // 39. LINQ support in Resharper and Rider
         }
     }
+    public class Human
+    {
+        public string Name;
+        public int Age;
+    }
+
 }
