540f80a40ac9c7d86ed35f87cd59639be7b5e5b7
awais <asaeed897@outlook.com>
2020-06-12 14:31:04 +0500

    Completed Lesson 14. Sorting Data Orderby, Thenby and Reverse

--- MasteringLINQWithCSharpCourse/Program.cs
+++ MasteringLINQWithCSharpCourse/Program.cs
@@ -12,7 +12,7 @@ namespace MasteringLINQWithCSharpCourse
         static void Main(string[] args)
         {
             
-            // 13. Filtering Data OfType and Where
+            // 14. Sorting Data Orderby, Thenby and Reverse
 
         }
     }
