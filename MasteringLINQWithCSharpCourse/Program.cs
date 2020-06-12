1005ca71ab1c61d549cf0a7fc1fb3c1d5bfaac02
awais <asaeed897@outlook.com>
2020-06-12 14:08:48 +0500

    Completed Lesson 12 Projection Operation Select and SelectMany

--- MasteringLINQWithCSharpCourse/Program.cs
+++ MasteringLINQWithCSharpCourse/Program.cs
@@ -12,7 +12,7 @@ namespace MasteringLINQWithCSharpCourse
         static void Main(string[] args)
         {
             
-            // 11. Converting Data Types (Cast)
+            // 12. Projection Operation Select and SelectMany
 
         }
     }
