bc873c0d096db063d9831eb8eb9d8fa168156544
awais <asaeed897@outlook.com>
2020-06-12 21:52:06 +0500

    Completed Lesson 32 AsParallel and ParallelQuery

--- MasteringLINQWithCSharpCourse/Program.cs
+++ MasteringLINQWithCSharpCourse/Program.cs
@@ -12,8 +12,32 @@ namespace MasteringLINQWithCSharpCourse
     {
         static void Main(string[] args)
         {
+            const int count = 50;
 
-            // 31. Overview Section 4 
+            var items = Enumerable.Range(1, count).ToArray();
+            var results = new int[count];
+
+            items.AsParallel().ForAll(x =>
+            {
+                int newValue = x * x * x ;
+                Console.Write($"{newValue} ({Task.CurrentId})\t");
+                results[x - 1] = newValue;
+            });
+            Console.WriteLine();
+            Console.WriteLine();
+            //
+            // foreach (var i in results)
+            // {
+            //     Console.WriteLine($"{i}\t");
+            // }
+
+            var cubes = items.AsParallel().AsOrdered().Select(x => x * x * x);
+
+            foreach (var i in results)
+            {
+                Console.Write($"{i}\t");
+            }
+            // 32. AsParallel and ParallelQuery
         }
     }
 }
