24562795407dc05adda10adf665e9ceec555f431
awais <asaeed897@outlook.com>
2020-06-12 23:39:51 +0500

    Completed Lesson 41 Course Summary Course Completed

--- MasteringLINQWithCSharpCourse/Program.cs
+++ MasteringLINQWithCSharpCourse/Program.cs
@@ -13,66 +13,9 @@ namespace MasteringLINQWithCSharpCourse
 
     class Program
     {
-        public Program()
-        {
-            //BatchDemo();
-            //InterleaveDemo();
-            //PermDemo();
-            SplitDemo();
-        }
-
-        private void SplitDemo()
-        {
-            var rand = new Random();
-            var numbers = Enumerable.Range(1, 100).Select(_ => rand.Next(10));
-
-            var split = numbers.Split(5);
-            foreach (var group in split)
-                Console.WriteLine($"{group.Count()} elements :" +
-                                  string.Join(", ", group));
-        }
-
-        private void PermDemo()
-        {
-            char[] letters = "draw".ToCharArray();
-
-            foreach (var item in letters.Permutations())
-            {
-                Console.WriteLine(new string(item.ToArray()));
-            }
-        }
-
-        private void InterleaveDemo()
-        {
-            var rand = new Random();
-            var wholeNumbers = Enumerable.Range(1, 10).Select(_ => (double)rand.Next(10));
-            var fractNumbers = Enumerable.Range(1, 10).Select(_ => rand.NextDouble());
-
-            foreach (var d in wholeNumbers.Interleave(fractNumbers))
-            {
-                Console.Write(d);
-                Console.Write("\t");
-            }
-            Console.WriteLine();
-        }
-
-        private void BatchDemo()
-        {
-            var numbers = Enumerable.Range(1, 100);
-            foreach (var batch in numbers.Batch(10))
-            {
-                Console.WriteLine("Got a batch!");
-                foreach (var i in batch)
-                    Console.Write($"{i}\t");
-                Console.WriteLine();
-            }
-        }
         static void Main(string[] args)
         {
-            new Program();
-            // 40. MoreLINQ
-
-            // This is a extension and have many useful features 
+            // 41. Course Summary
         }
     }
     
